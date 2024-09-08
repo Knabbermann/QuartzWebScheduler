33329581a33a60ed6a1909e3e43ea87656fe46ed
Jiri Skoda <jiri.skoda@gmx.de>
2024-09-08 15:15:23 +0200

    added theme, darkmode and png/icon

---
+++ QuartzWebScheduler.Web/Pages/Shared/_DarkTheme.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HomePost8.Web.Pages.Shared
+{
+    public class _DarkThemeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
