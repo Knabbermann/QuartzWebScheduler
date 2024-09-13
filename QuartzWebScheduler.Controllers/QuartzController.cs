c54883aac03ea65b0d40e3ff0361fdb3d19f1ade
Jiri Skoda <jiri.skoda@gmx.de>
2024-09-13 10:10:54 +0200

    changed quartzcontroller to loading jobconfigs from db

--- QuartzWebScheduler.Controllers/QuartzController.cs
+++ QuartzWebScheduler.Controllers/QuartzController.cs
@@ -2,15 +2,28 @@
 using Quartz;
 using QuartzWebScheduler.Controllers.Interfaces;
 using QuartzWebScheduler.Utilities;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using QuartzWebScheduler.Models;
+using Microsoft.Data.SqlClient;
 
 namespace QuartzWebScheduler.Controllers
 {
     public class QuartzController : IQuartzController
     {
         private readonly IScheduler _scheduler;
+        private readonly string _connectionString;
 
         public QuartzController()
         {
+            if (_connectionString.IsNullOrEmpty())
+            {
+                var configurationBuilder = new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+
+                _connectionString = configurationBuilder.GetConnectionString("WebDbContextConnection");
+            }
             // Scheduler erstellen
             var schedulerFactory = new StdSchedulerFactory();
             _scheduler = schedulerFactory.GetScheduler().Result;
@@ -20,29 +33,65 @@ namespace QuartzWebScheduler.Controllers
         public async Task StartSchedulerAsync()
         {
             await _scheduler.Start();
+            var jobConfigs = await GetQuartzJobConfigsAsync();
+            foreach (var jobConfig in jobConfigs)
+            {
+                // Erstelle den Job
+                IJobDetail job = JobBuilder.Create<QuartzJob>() // Du kannst hier den eigentlichen Job-Typ einsetzen
+                    .WithIdentity(jobConfig.JobName, "group1")
+                    .UsingJobData("Url", jobConfig.Url) // Übergabe von Daten an den Job
+                    .Build();
 
-            // Definiere den Job
-            IJobDetail job = JobBuilder.Create<QuartzJob>()
-                .WithIdentity("myJob", "group1")
-                .Build();
+                // Erstelle den Trigger basierend auf der CronExpression aus der Konfiguration
+                ITrigger trigger = TriggerBuilder.Create()
+                    .WithIdentity($"{jobConfig.JobName}_Trigger", "group1")
+                    .WithCronSchedule(jobConfig.CronExpression) // Verwendet die CronExpression aus der Config
+                    .Build();
 
-            // Definiere den Trigger (alle 5 Sekunden ausführen)
-            ITrigger trigger = TriggerBuilder.Create()
-                .WithIdentity("myTrigger", "group1")
-                .StartNow()
-                .WithSimpleSchedule(x => x
-                    .WithIntervalInSeconds(5)
-                    .RepeatForever())
-                .Build();
-
-            // Job mit dem Trigger dem Scheduler hinzufügen
-            await _scheduler.ScheduleJob(job, trigger);
+                // Füge den Job mit dem Trigger dem Scheduler hinzu
+                await _scheduler.ScheduleJob(job, trigger);
+            }
         }
+
 
         // Stoppt den Scheduler
         public async Task StopSchedulerAsync()
         {
             await _scheduler.Shutdown();
         }
+
+        private async Task<List<QuartzJobConfig>> GetQuartzJobConfigsAsync()
+        {
+            var jobConfigs = new List<QuartzJobConfig>();
+
+            string query = "SELECT Id, JobName, CronExpression, Url FROM QuartzJobConfigs";
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                await connection.OpenAsync();
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = await command.ExecuteReaderAsync())
+                    {
+                        while (await reader.ReadAsync())
+                        {
+                            var jobConfig = new QuartzJobConfig
+                            {
+                                Id = reader["Id"].ToString(),
+                                JobName = reader["JobName"].ToString(),
+                                CronExpression = reader["CronExpression"].ToString(),
+                                Url = reader["Url"].ToString()
+                            };
+
+                            jobConfigs.Add(jobConfig);
+                        }
+                    }
+                }
+            }
+
+            return jobConfigs;
+        }
+
     }
 }
